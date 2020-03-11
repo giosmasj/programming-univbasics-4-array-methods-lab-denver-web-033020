2f28d8563c0865d9e4e1b5cbccc29a9fa68e5f66
James Giosmas <giosmasj@gmail.com>
2020-03-11 02:31:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-  
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  aaray.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  arraylast
 end
 
 def using_size(array)
-
+  array.size
 end
